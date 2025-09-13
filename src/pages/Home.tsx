3fe184743239ec00b1e9ae1fb8faa2b3adc03b2f
achiappone <anthonychiappone@gmail.com>
2025-09-13 17:22:14 -0400

    last update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <Typography variant="h4" gutterBottom>Software Portfolio</Typography>
       <Typography>
         Front-end and full-stack projects focused on React, TypeScript, data visualization, and PDF export.
+        Skills include 
       </Typography>
     </Box>
   );
