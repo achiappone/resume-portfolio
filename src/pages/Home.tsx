2eaeb801ba47efcc9baef3f254d9ac5a9f63d3d6
achiappone <anthonychiappone@gmail.com>
2025-10-02 21:38:38 -0400

    dates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,12 +18,15 @@ export default function Home() {
         This portfolio showcases Anthony's software development projects. These are primarily
         Front-end and full-stack projects focused on React, TypeScript, data visualization, and PDF export.
       </Typography>
+      
       <Typography paragraph>
         Software Skills include: React, TypeScript, React Native, Node.js, MobX-State-Tree, MySQL, C++, C#, Python, .NET, Networking.
         General Skills include: Project Management (Agile methodologies), Database Design, Product Management, Market Research, and Technical Writing.
-      
+      </Typography>
+
+      <Typography paragraph>
         Additionally, Anthony has a background in product management for pro lighting/video systems. Anthony has extensive experience leading cross-functional teams to deliver products on time and on budget.
-        He has also built internal apps for quoting and spec PDFs, and led software teams using Agile methodologies.
+        He has also built internal apps for functional sales function purposes, and led software teams using Agile methodologies.
         Anthony is passionate about the work he does and is always looking for new challenges and opportunities to learn and grow.
       </Typography>
 
